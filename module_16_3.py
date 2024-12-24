f27f2a3ba8f9f5c044a54a314cc7d67e417cb0fd
ak-online <79262567597@yandex.com>
2024-12-24 15:14:25 +0300

    Add files via upload

---
+++ module_16_3.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI, Path
+from typing import Annotated
+
+app = FastAPI()
+users = {'1': 'Имя: Example, возраст: 18'}
+
+
+@app.get("/users")
+async def get_all_users() -> dict:
+    return users
+
+@app.post("/user/{username}/{age}")
+async def create_user(username: Annotated[str, Path(min_length=5, max_length=20,
+                                                    description="Enter Username", example="UrbanUser")]
+               , age: int = Path(ge=18, le=120, description="Enter Age")) -> str:
+    user_id = str(int(max(users, key = int)) + 1)
+    users[user_id] = f"Имя : {username}, возраст: {age}"
+    return (f"User {user_id} is registered")
+
+@app.put('/user/{user_id}/{username}/{age}')
+async def update_users(username: Annotated[str, Path(min_length=5, max_length=20,
+                                                     description='Enter username', example='UrbanUser')],
+                       user_id: str = Path(min_length=1, max_length=100,
+                                           description='Enter User ID', example='1'),
+                       age: int = Path(ge=18, le=120, description='Enter age', example='24')) -> str:
+
+    users[user_id] = f"Имя : {username}, возраст: {age}"
+    return f'User {user_id} has been updated.'
+
+@app.delete("/user/{user_id}")
+async def delete_message(user_id : str) -> str:
+    users.pop(user_id)
+    return f'User with ID {user_id} deleted'
